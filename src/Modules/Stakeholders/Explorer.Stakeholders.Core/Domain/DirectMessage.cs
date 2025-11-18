bf51f62bfae92633866fa3945c7b2ff08fe67b5a
DarkChill42 <borisbodvanski@gmail.com>
2025-11-18 14:43:23 +0100

    Implemented domain entity for messages and repository interface

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/DirectMessage.cs
@@ -0,0 +1,38 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class DirectMessage : Entity
+    {
+        public long SenderId { get; set; }
+        public long RecipientId { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+        public DateTime? EditedAt { get; set; }
+
+        public DirectMessage() { }
+
+        public DirectMessage(long senderId, long recipientId, string content, DateTime sentAt)
+        {
+            SenderId = senderId;
+            RecipientId = recipientId;
+            Content = content;
+            SentAt = sentAt;
+            EditedAt = null;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (SenderId == 0 || RecipientId == 0) throw new ArgumentException("Invalid UserId");
+            if (string.IsNullOrWhiteSpace(Content)) throw new ArgumentException("Invalid Message");
+        }
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IDirectMessageRepository.cs
@@ -0,0 +1,17 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain.RepositoryInterfaces
+{
+    public interface IDirectMessageRepository
+    {
+        PagedResult<DirectMessage> GetPaged(int page, int pageSize);
+        DirectMessage Create(DirectMessage map);
+        DirectMessage Update(DirectMessage map);
+        void Delete(long id);
+    }
+}
