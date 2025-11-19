d1dc7b70ff5b4c444bb404d7404b77563e889082
Marko Sindjic <sindjic.marko03@gmail.com>
2025-11-19 15:09:05 +0100

    feat: added user profile DTOs, service interface, domain model and mappings

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Mappers/StakeholderProfile.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Mappers/StakeholderProfile.cs
@@ -9,5 +9,9 @@ public class StakeholderProfile : Profile
     public StakeholderProfile()
     {
         CreateMap<Club, ClubDto>().ReverseMap();
+        CreateMap<UserProfile, UserProfileDto>().ReverseMap();
+        CreateMap<UpdateUserProfileDto, UserProfile>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore());
     }
 }
