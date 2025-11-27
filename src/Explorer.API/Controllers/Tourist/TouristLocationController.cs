e48f1e4a5068176874a163c923c8eaf3e0b3758c
Bojan Grujicic <bojangrujicic5@gmail.com>
2025-11-27 07:21:10 +0100

    feat/dodat TouristLocation

---
+++ src/Explorer.API/Controllers/Tourist/TouristLocationController.cs
@@ -0,0 +1,36 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Stakeholders.Core.UseCases;
+using Explorer.Stakeholders.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/tourist/location")]
+    [ApiController]
+    public class TouristLocationController : ControllerBase
+    {
+        private readonly ITouristLocationService _service;
+
+        public TouristLocationController(ITouristLocationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<TouristLocationDto> GetMine()
+        {
+            var result = _service.Get(User.PersonId());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public ActionResult<TouristLocationDto> Update([FromBody] TouristLocationDto dto)
+        {
+            var updated = _service.SaveOrUpdateLocation(User.PersonId(), dto);
+            return Ok(updated);
+        }
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/TouristLocationDbRepository.cs
@@ -0,0 +1,38 @@
+﻿using Explorer.Stakeholders.Core.Domain;
+using Explorer.Stakeholders.Core.Domain.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Explorer.Stakeholders.Infrastructure.Database.Repositories
+{
+    public class TouristLocationDbRepository : ITouristLocationRepository
+    {
+        private readonly StakeholdersContext _dbContext;
+        private readonly DbSet<TouristLocation> _dbSet;
+
+        public TouristLocationDbRepository(StakeholdersContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<TouristLocation>();
+        }
+
+        public TouristLocation? GetById(long userId)
+        {
+            return _dbSet.FirstOrDefault(l => l.UserId == userId);
+        }
+
+        public TouristLocation Save(TouristLocation entity)
+        {
+            if (entity.Id == 0)
+            {
+                _dbSet.Add(entity);
+            }
+            else
+            {
+                _dbSet.Update(entity);
+            }
+
+            _dbContext.SaveChanges();
+            return entity;
+        }
+    }
+}
