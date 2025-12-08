90a3bc0a6dbdb67a7324d37d6b1fecb22d7dacbe
jelenasumar <sumar.ra94.2022@uns.ac.rs>
2025-12-08 19:53:43 +0100

    feat: add script for tour execution

---
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/h-tour-executions.sql
@@ -0,0 +1,16 @@
+INSERT INTO tours."TourExecutions"(
+    "Id", "TouristId", "TourId", "StartTime", "StartLatitude", "StartLongitude", "Status", "EndTime", "LastActivity")
+VALUES (-1, -21, -2, '2024-01-15 10:00:00+00', 45.2671, 19.8335, 0, NULL, '2024-01-15 10:00:00+00');
+
+INSERT INTO tours."TourExecutions"(
+    "Id", "TouristId", "TourId", "StartTime", "StartLatitude", "StartLongitude", "Status", "EndTime", "LastActivity")
+VALUES (-2, -21, -2, '2024-01-16 14:30:00+00', 45.2671, 19.8335, 1, '2024-01-16 16:45:00+00', '2024-01-16 16:45:00+00');
+
+INSERT INTO tours."TourExecutions"(
+    "Id", "TouristId", "TourId", "StartTime", "StartLatitude", "StartLongitude", "Status", "EndTime", "LastActivity")
+VALUES (-3, -22, -1, '2024-01-17 09:15:00+00', 45.2671, 19.8335, 2, '2024-01-17 10:30:00+00', '2024-01-17 10:30:00+00');
+
+INSERT INTO tours."TourExecutions"(
+    "Id", "TouristId", "TourId", "StartTime", "StartLatitude", "StartLongitude", "Status", "EndTime", "LastActivity")
+VALUES (-4, -22, -3, '2024-01-18 11:20:00+00', 45.2671, 19.8335, 0, NULL, '2024-01-18 11:20:00+00');
+
