2a0e55c71d253d079ad823ffd29541603f78465d
Marko Sindjic <sindjic.marko03@gmail.com>
2025-12-11 22:49:42 +0100

    feat/tour-search-results add tour-search Dtos, Service, Tests

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourSearchRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class TourSearchRequestDto
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public double RadiusKm { get; set; }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourSearchResultDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class TourSearchResultDto
+    {
+        public int TourId { get; set; }
+        public string Name { get; set; } = "";
+        public string ShortDescription { get; set; } = "";
+        public MatchingPointDto MatchingPoint { get; set; } = new();
+    }
+
+    public class MatchingPointDto
+    {
+        public int KeyPointId { get; set; }
+        public string Title { get; set; } = "";
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public double DistanceKm { get; set; }
+    }
+}
+
