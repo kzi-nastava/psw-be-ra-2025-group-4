8bde73a7b9502cd08f75b76393417447a4e76755
Katarina Zgonjanin <katarinazgonjanin@gmail.com>
2025-12-12 00:17:43 +0100

    fix:  enable camelCase JSON serialization and enum string conversion

--- src/Modules/Blog/Explorer.Blog.Core/Mappers/BlogProfile.cs
+++ src/Modules/Blog/Explorer.Blog.Core/Mappers/BlogProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Explorer.Blog.API.Dtos;
 using Explorer.Blog.Core.Domain;
+using System.Text.Json.Serialization;
 
 namespace Explorer.Blog.Core.Mappers;
 
@@ -8,11 +9,14 @@ public class BlogProfile : Profile
 {
     public BlogProfile()
     {
-        CreateMap<DigitalDiaryDto, DigitalDiary>()
-                .ReverseMap();
+        CreateMap<DigitalDiaryDto, DigitalDiary>().ReverseMap();
+
         CreateMap<BlogPost, BlogDto>()
-                        .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
-                        .ForMember(dest => dest.LastUpdatedAt, opt => opt.MapFrom(src => src.LastUpdatedAt));
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (BlogStatusDTO)src.Status))
+            .ForMember(dest => dest.Popularity, opt => opt.MapFrom(src => (BlogPopularityDTO)src.Popularity))
+            .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.Images))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+            .ForMember(dest => dest.LastUpdatedAt, opt => opt.MapFrom(src => src.LastUpdatedAt));
 
         CreateMap<CreateUpdateBlogDto, BlogPost>()
             .ForCtorParam("images", opt => opt.MapFrom(src => src.Images));
