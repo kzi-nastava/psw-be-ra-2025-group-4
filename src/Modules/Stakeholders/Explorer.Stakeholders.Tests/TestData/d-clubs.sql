365455bf5adb5b1ef22ad8c070aa636817301823
Bojan Grujicic <bojangrujicic5@gmail.com>
2025-12-18 03:06:33 +0100

    fix: testovi

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubs.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubs.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO stakeholders."Clubs"("Id", "Name", "Description", "OwnerId", "ImageUrls")
+﻿INSERT INTO stakeholders."Clubs"("Id", "Name", "Description", "OwnerId", "ImageUrls", "Status", "Members", "InvitedTourist", "RequestedTourists")
 VALUES 
-    (-1, 'Test Club 1', 'Opis testnog kluba 1', -21, ARRAY['test']),
-    (-2, 'Test Club 2', 'Opis testnog kluba 2', -21, ARRAY['test']);
+    (-1, 'Test Club 1', 'Opis testnog kluba 1', -21, ARRAY['test'], 0, ARRAY[]::bigint[], ARRAY[]::bigint[], ARRAY[]::bigint[]),
+    (-2, 'Test Club 2', 'Opis testnog kluba 2', -21, ARRAY['test'], 1, ARRAY[]::bigint[], ARRAY[]::bigint[], ARRAY[]::bigint[]);
