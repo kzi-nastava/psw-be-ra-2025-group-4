fe2692ffd46404250af21ae282bdb1d061292c52
milicevicteodora9 <milicevic.ra89.2022@uns.ac.rs>
2025-12-24 14:35:09 +0100

    feat: add popular tours endpoint for authors

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/PopularTourDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Explorer.Tours.API.Dtos;
+
+public class PopularTourDto
+{
+    public long TourId { get; set; }
+    public string Name { get; set; } = "";
+    public TourDtoStatus Status { get; set; }
+
+
+    public string? Area { get; set; }
+
+  
+    public double PopularityScore { get; set; }
+    public double AverageGrade { get; set; }
+    public int RatingsCount { get; set; }
+}
