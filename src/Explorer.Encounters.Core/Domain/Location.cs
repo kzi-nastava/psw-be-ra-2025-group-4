2110ab49424e141391a81d5dff38b14c644013f0
DarkChill42 <borisbodvanski@gmail.com>
2025-12-25 21:17:32 +0100

    feat: Implemented aggregate for encounter model and it's value objects. Implemented interface repository for encounter

---
+++ src/Explorer.Encounters.Core/Domain/Location.cs
@@ -0,0 +1,39 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Core.Domain
+{
+    public class Location : ValueObject
+    {
+        public double Longitude { get; private set; }
+        public double Latitude { get; private set; }
+
+        private Location()
+        {
+
+        }
+
+        [JsonConstructor]
+        public Location(double longitude, double latitude)
+        {
+            if (latitude < -90 || latitude > 90)
+                throw new ArgumentOutOfRangeException(nameof(latitude), "Latitude must be between -90 and 90 degrees.");
+            if (longitude < -180 || longitude > 180)
+                throw new ArgumentOutOfRangeException(nameof(longitude), "Longitude must be between -180 and 180 degrees.");
+
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Longitude;
+            yield return Latitude;
+        }
+    }
+}
