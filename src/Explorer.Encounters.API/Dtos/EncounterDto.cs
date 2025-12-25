21bb985444f83d77ec1f5be967a926fb188ce730
DarkChill42 <borisbodvanski@gmail.com>
2025-12-25 21:21:11 +0100

    feat: Implemented DTO classes for model classes. Implemented encounter service interface

---
+++ src/Explorer.Encounters.API/Dtos/EncounterDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.API.Dtos
+{
+    public enum EncounterStatus
+    {
+        Active,
+        Draft,
+        Archived
+    }
+
+    public enum EncounterType
+    {
+        Social,
+        Location,
+        Misc
+    }
+
+    public class EncounterDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public LocationDto Location { get; set; }
+        public int ExperiencePoints { get; set; }
+        public EncounterStatus Status { get; set; }
+        public EncounterType Type { get; set; }
+    }
+}
