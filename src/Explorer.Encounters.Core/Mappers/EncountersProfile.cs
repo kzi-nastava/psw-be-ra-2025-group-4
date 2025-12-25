605b35874eb6b7f66929aa15012db4cc07b1f15f
game896 <gameing40687@gmail.com>
2025-12-25 16:41:54 +0100

    Added Encouters module

---
+++ src/Explorer.Encounters.Core/Mappers/EncountersProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Explorer.Encounters.Core.Mappers;
+
+public class EncountersProfile : Profile
+{
+    public EncountersProfile()
+    {
+
+    }
+}
---
+++ src/Explorer.Encounters.Infrastructure/EncountersStartup.cs
@@ -0,0 +1,38 @@
+﻿using Explorer.BuildingBlocks.Infrastructure.Database;
+using Explorer.Encounters.Core.Mappers;
+using Explorer.Encounters.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace Explorer.Encounters.Infrastructure
+{
+    public static class EncountersStartup
+    {
+        public static IServiceCollection ConfigureEncountersModule(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(EncountersProfile).Assembly);
+            SetupCore(services);
+            SetupInfrastructure(services);
+            return services;
+        }
+
+        private static void SetupCore(IServiceCollection services)
+        {
+            //TODO
+        }
+
+        private static void SetupInfrastructure(IServiceCollection services)
+        {
+            //TODO
+
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(DbConnectionStringBuilder.Build("encounters"));
+            dataSourceBuilder.EnableDynamicJson();
+
+            var dataSource = dataSourceBuilder.Build();
+
+            services.AddDbContext<EncountersContext>(opt =>
+            opt.UseNpgsql(dataSource, x => x.MigrationsHistoryTable("__EFMigrationsHistory", "encounters")));
+        }
+    }
+}
