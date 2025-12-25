4afe523ea502333e07c61258cb54ab542caefacd
DarkChill42 <borisbodvanski@gmail.com>
2025-12-25 21:22:13 +0100

    feat: Implemented basic CRUD HTTP controller methods for encounters module

---
+++ src/Explorer.API/Controllers/Tourist/Encounters/TouristEncountersController.cs
@@ -0,0 +1,26 @@
+﻿using Explorer.Encounters.API.Dtos;
+using Explorer.Encounters.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist.Encounters
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/tourist/encounters")]
+    [ApiController]
+    public class TouristEncountersController : ControllerBase
+    {
+        private IEncounterService _encounterService;
+
+        public TouristEncountersController(IEncounterService encounterService)
+        {
+            _encounterService = encounterService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<EncounterDto>> GetActive()
+        {
+            return Ok(_encounterService.GetActive());
+        }
+    }
+}
