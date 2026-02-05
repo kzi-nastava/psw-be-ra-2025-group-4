3a629ed6acdb638f79f2fa3f9f3524483dd05df3
Marko Sindjic <sindjic.marko03@gmail.com>
2026-02-05 18:59:05 +0100

    add tests

---
+++ src/Modules/Tours/Explorer.Tours.Tests/Unit/MysteryTourOfferTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Explorer.Tours.Core.Domain;
+using Shouldly;
+using Xunit;
+
+namespace Explorer.Tours.Tests.Unit
+{
+    public class MysteryTourOfferTests
+    {
+        [Fact]
+        public void Constructor_sets_default_values()
+        {
+            var offer = new MysteryTourOffer(touristId: 5, tourId: 12, discountPercent: 20);
+
+            offer.Id.ShouldNotBe(Guid.Empty);
+            offer.TouristId.ShouldBe(5);
+            offer.TourId.ShouldBe(12);
+            offer.DiscountPercent.ShouldBe(20);
+
+            offer.Redeemed.ShouldBeFalse();
+            offer.CreatedAt.ShouldBeLessThanOrEqualTo(DateTime.UtcNow);
+            offer.ExpiresAt.ShouldBe(offer.CreatedAt.AddMinutes(10));
+        }
+
+        [Fact]
+        public void Redeem_marks_offer_as_redeemed_when_valid()
+        {
+            var offer = new MysteryTourOffer(1, 10, 30);
+
+            offer.Redeem();
+
+            offer.Redeemed.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Redeem_throws_when_already_redeemed()
+        {
+            var offer = new MysteryTourOffer(1, 10, 30);
+            offer.Redeem();
+
+            var ex = Should.Throw<InvalidOperationException>(() => offer.Redeem());
+            ex.Message.ShouldBe("Offer already redeemed.");
+        }
+
+        [Fact]
+        public void Redeem_throws_when_expired()
+        {
+            var offer = new MysteryTourOffer(1, 10, 30);
+
+            typeof(MysteryTourOffer)
+                .GetProperty(nameof(MysteryTourOffer.ExpiresAt))!
+                .SetValue(offer, DateTime.UtcNow.AddMinutes(-1));
+
+            var ex = Should.Throw<InvalidOperationException>(() => offer.Redeem());
+            ex.Message.ShouldBe("Offer expired.");
+        }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Tests/Unit/TourTransportDurationTests.cs
@@ -0,0 +1,33 @@
+﻿using Explorer.Tours.Core.Domain;
+using Shouldly;
+using Xunit;
+
+namespace Explorer.Tours.Tests.Unit
+{
+    public class TourTransportDurationTests
+    {
+        [Fact]
+        public void Same_values_should_be_equal()
+        {
+            // Arrange
+            var d1 = new TourTransportDuration(30, TourTransportType.Foot);
+            var d2 = new TourTransportDuration(30, TourTransportType.Foot);
+
+            // Act & Assert
+            d1.ShouldBe(d2);
+            d1.Equals(d2).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Different_values_should_not_be_equal()
+        {
+            // Arrange
+            var d1 = new TourTransportDuration(30, TourTransportType.Foot);
+            var d2 = new TourTransportDuration(45, TourTransportType.Bike);
+
+            // Act & Assert
+            d1.ShouldNotBe(d2);
+            d1.Equals(d2).ShouldBeFalse();
+        }
+    }
+}
